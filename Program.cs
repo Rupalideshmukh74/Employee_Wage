9dffcc1fbcc09933bf334fe7e9ff5bba9dac28a1
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-04 11:42:01 +0530

    [Rupali]Refactor:Checked employee is present or not

--- Program.cs
+++ Program.cs
@@ -10,7 +10,19 @@ namespace EmployeePayRole
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to employee wage problem ");
+            // Console.WriteLine("Welcome to employee wage problem ");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int isPresent = random.Next(0, 2);
+            Console.WriteLine(isPresent);
+           // if (isPresent== IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
