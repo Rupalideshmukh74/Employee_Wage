bf664dc290242dc60b591dc17f54929a34de4fd2
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-04 12:04:10 +0530

    [Rupali]Refactor:Calculated daily employee wage

--- Program.cs
+++ Program.cs
@@ -12,17 +12,23 @@ namespace EmployeePayRole
         {
             // Console.WriteLine("Welcome to employee wage problem ");
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HR = 1000;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int isPresent = random.Next(0, 2);
             Console.WriteLine(isPresent);
-           // if (isPresent== IS_FULL_TIME)
+            if (isPresent== IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
             }
+            empWage = empHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("Employee wage: " + empWage);
         }
     }
 }
