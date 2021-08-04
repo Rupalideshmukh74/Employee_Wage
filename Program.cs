b190346b450ee1a5daa45568e7639e1f74c4af18
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-04 14:45:36 +0530

    [Rupali]Refactor:Added_part_Time_wage

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace EmployeePayRole
             // Console.WriteLine("Welcome to employee wage problem ");
             int IS_FULL_TIME = 1;
             int EMP_RATE_PER_HR = 1000;
+            int PART_TIME_HR = 4;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
@@ -20,12 +21,13 @@ namespace EmployeePayRole
             Console.WriteLine(isPresent);
             if (isPresent== IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is present");
+                Console.WriteLine("Employee is present full time");
                 empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                Console.WriteLine("Employee is present part time");
+                empHrs = 4;
             }
             empWage = empHrs * EMP_RATE_PER_HR;
             Console.WriteLine("Employee wage: " + empWage);
