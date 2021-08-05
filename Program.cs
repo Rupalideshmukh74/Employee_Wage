0cd4ce1835ec84eca2c52e4a8247f44b51129ce7
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-05 13:50:18 +0530

    [Rupali]Refactor:calculated wage till condition is reached

--- Program.cs
+++ Program.cs
@@ -12,8 +12,7 @@ namespace EmployeePayRole
         {
            
             int PER_HOUR_WAGE = 1000;
-            int HOURS_WORK = 8;
-            int DAILY_WAGE = 0;
+            
             int daysWorked = 0;
             int PART_TIME = 1;
             int monthlyWage = 0;
@@ -22,7 +21,7 @@ namespace EmployeePayRole
             int counterForWhile = 0;
             Random rand = new Random();
 
-            for (int i = 0; i < 20; i++)
+            while (noOfDayHoursWorked <= 100 && daysWorked <= 20)
             {
                 dailywage = 0;
                 int workstatus = rand.Next(0, 3);
@@ -38,13 +37,14 @@ namespace EmployeePayRole
                     case 2:
                         Console.WriteLine("Employee has worked part time");
                         dailywage = PER_HOUR_WAGE * PART_TIME;
+                        daysWorked++;
                         break;
                     default:
                         Console.WriteLine("something wrong happened");
                         break;
                 }
                 monthlyWage = monthlyWage + dailywage;
-                Console.WriteLine("Total daily wage earned for day " + i + " " + dailywage);
+                Console.WriteLine("Total daily wage earned for day " + " " + dailywage);
                 Console.WriteLine("No of Hours Worked" + noOfDayHoursWorked);
                 monthlyWage += dailywage;
                 counterForWhile++;
